33c6f7e1d3a2cd3b3f5a62cf34ec1504b42b8e13
caojen <caojen@mail2.sysu.edu.cn>
2024-12-13 23:33:20 +0800

    examples: add basic

---
+++ examples/basic.rs
@@ -0,0 +1,31 @@
+use tokio::time;
+use async_context::{Error, Timer, With};
+
+/// In this file, we send a HTTP GET request to https://www.example.com.
+/// We use context to handle slow network, and kill the request after 3 seconds.
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // give only 3 seconds to finish the request
+    let timer = Timer::with_timeout(time::Duration::from_secs(3));
+
+    let url = "https://www.example.com";
+
+    let response = reqwest::Client::new()
+        .get(url)
+        .send()
+        .with(timer.clone()) // clone:
+        .await;
+
+    match response {
+        Ok(Ok(response)) => println!("successfully request: {:?}", response),
+        Ok(Err(err)) => println!("request error from reqwest: {:?}", err),
+        Err(err) => match err {
+            Error::ContextTimeout => println!("request timeout: {}", err),
+            Error::ContextCancelled => println!("request cancelled: {}", err),
+            _ => unimplemented!(),
+        }
+    }
+
+    Ok(())
+}
