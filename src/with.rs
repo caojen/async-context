71b1a7ca9c870aac94057a330700c134389d556c
caojen <caojen@mail2.sysu.edu.cn>
2024-12-14 03:10:42 +0800

    feat(v0.2.1): remove Future: Sync for With

--- src/with.rs
+++ src/with.rs
@@ -5,17 +5,17 @@ use crate::Context;
 pub trait With<Output> {
     async fn with<Ctx>(self, ctx: Ctx) -> crate::Result<Output>
     where
-        Ctx: Context + Send + Sync;
+        Ctx: Context + Send;
 }
 
 #[async_trait::async_trait]
 impl<'a, Output, Fut> With<Output> for Fut
 where
-    Fut: Future<Output = Output> + Send + Sync + 'a
+    Fut: Future<Output = Output> + Send + 'a
 {
     async fn with<Ctx>(self, ctx: Ctx) -> crate::Result<Output>
     where
-        Ctx: Context + Send + Sync,
+        Ctx: Context + Send,
     {
         ctx.handle(self).await
     }
